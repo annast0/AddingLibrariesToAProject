95e1318aac0507403f12eed41bdffb6cb0b6f03e
Anastasia <annaniko94@gmail.com>
2024-09-25 21:21:56 +0300

    Nikolaenko_Anastasia_PR-22.101

---
+++ app/src/main/java/com/example/addinglibrariestoaproject/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.addinglibrariestoaproject
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.addinglibrariestoaproject.ui.theme.AddingLibrariesToAProjectTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AddingLibrariesToAProjectTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Nikolaenko ПР-22.101",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "$name",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AddingLibrariesToAProjectTheme {
+        Greeting("Nikolaenko ПР-22.101")
+    }
+}
